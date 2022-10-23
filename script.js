f73a0b684a0216738fdfa977ee180be48bc99c90
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-10-23 00:15:57 -0500

    button color = velocity

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 Tone.Transport.bpm.value = 120
-const Major = [0,2,4,5,7,9,11,12];
+const Major = [0, 2, 4, 5, 7, 9, 11, 12];
 const startingNote = 60
 
 // randArr = Array.from(
@@ -8,14 +8,15 @@ const startingNote = 60
 //     console.log('clicked')
 
 let kickArrEl = document.getElementById('kick-pattern')
+let kickStepsEl = document.getElementById('kick-steps')
 let startEl = document.getElementById('start')
 let stopEl = document.getElementById('stop')
-let CMajor =[]
+let CMajor = []
 
-for(let i=0; i<Major.length; i++) {
-    CMajor.push( Major[i] + startingNote );
+for (let i = 0; i < Major.length; i++) {
+    CMajor.push(Major[i] + startingNote);
 }
-console.log(CMajor)
+
 
 const kickDrum = new Tone.MembraneSynth({
     volume: 6,
@@ -28,27 +29,44 @@ let randArr = []
 let snareArr = []
 let kicks = []
 
+function rgb(r, g, b){
+    return ["rgb(",r,",",g,",",b,")"].join("");
+  }
 
-let randomIntFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1) + min) 
-  const rndInt = randomIntFromInterval(1, 8)
-  console.log(rndInt)
+let randomIntFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+const rndInt = randomIntFromInterval(1, 8)
+console.log(rndInt)
 
 
-  let x = Array(16).fill().map(
+let x = Array(16).fill().map(
     () => Array(3).fill().map(
-      () => Math.floor(Math.random() * 10)
+        () => Math.floor(Math.random() * 10)
     )
-  );
+);
 console.log(x)
+
+//for each increase in slider value add in a random note from note
+
+
 
 
 let setKickPattern = () => {
-
-  
-
-    Object.values(kicks).forEach(value => value.note = (CMajor[Math.floor(Math.random()*CMajor.length)]) );
+    Object.values(kicks).forEach(value => value.note = (CMajor[Math.floor(Math.random() * CMajor.length)]));
     //loops through kicks array and sets velocity to either on or off * random
-    Object.values(kicks).forEach(value => value.velocity = (Math.floor(Math.random()*2))*(Math.random() * 1));
+    Object.values(kicks).forEach(value => value.velocity = (Math.floor(Math.random() * 2)) * (Math.random() * 1));
+
+    Object.values(kicks).forEach(value => {
+        console.log(value.velocity)
+        console.log('hey')
+        let stepButton = document.createElement('button')
+        console.log(Object.values(kicks))
+        stepButton.style.background = rgb(255,value.velocity*255,50)
+        stepButton.innerText = 'hey'
+        kickStepsEl.appendChild(stepButton)
+
+
+    })
+
 }
 // const midi = Tone.Frequency("C3").toMidi();
 // console.log(midi);
@@ -71,7 +89,7 @@ kicks = [
     { time: '0:3:2', note: 32, velocity: 0 },
     { time: '0:3:3', note: 32, velocity: 0 },
 ]
-console.log(kicks)
+
 
 
 
@@ -82,10 +100,6 @@ console.log(kicks)
 
 const kickPart = new Tone.Part((time, value) => { kickDrum.triggerAttackRelease(value.note, '8n', time, value.velocity) },
     kicks).start(0);
-
-
-
-
 kickPart.loop = true
 
 
